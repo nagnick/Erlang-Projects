02ec78f72b3fa5d26300c2eb41d6d2e7c09a3856
nicolas <nicolasgarcia@ufl.edu>
2022-09-30 18:10:36 -0400

    Implemented a way to link actors in a line

--- src/project2.erl
+++ src/project2.erl
@@ -1,6 +1,37 @@
 
 -module(project2).
 
--export([print/0]).
-print()->
-  io:format("HELLOCake").
+-export([gossipActor/0,superVisor/0,spawnMultipleActors/1]).
+
+linkInLine(1,List)->
+  hd(List)! [self()],
+  io:format("DONE~n");
+linkInLine(NumberInList,List)->
+  PID = hd(List),
+  Rest = tl(List),
+  io:format("~p~n",[PID]),
+  PID ! [self()| hd(Rest)],
+  linkInLine(NumberInList-1,Rest).
+
+superVisor()->
+  Actors = spawnMultipleActors(8),
+  messageStructure(8,Actors,line).
+
+messageStructure(NumberOfActors,Actors, line)->
+  linkInLine(NumberOfActors,Actors).
+
+gossipActor()->
+  receive
+    ListOfNeighbors->
+      gossipActor(ListOfNeighbors)
+  end.
+gossipActor(ListOfNeighbors)->
+  io:format("~p",[ListOfNeighbors]).
+
+spawnMultipleActors(NumberOfActorsToSpawn)->
+  spawnMultipleActors(NumberOfActorsToSpawn,[]).
+spawnMultipleActors(0,ListOfPid)->
+  io:format("~p~n",[ListOfPid]),
+  ListOfPid;
+spawnMultipleActors(NumberOfActorsToSpawn,ListOfPid)->
+  spawnMultipleActors(NumberOfActorsToSpawn-1,[spawn(project2,gossipActor,[]) | ListOfPid]).
